73e4b3d42f7707db287e6a65cefc308cf1a4829b
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-04-18 19:43:55 -0500

    Fixes tournaments migration indentation

--- db/migrate/20200419002758_create_tournaments.rb
+++ db/migrate/20200419002758_create_tournaments.rb
@@ -1,14 +1,14 @@
 class CreateTournaments < ActiveRecord::Migration[5.2]
   def change
     create_table :tournaments do |t|
-    	t.string 	 :name
-    	t.datetime :start_date
-    	t.datetime :end_date
-    	t.text 		 :description
-    	t.integer  :type
-    	t.string 	 :country
-    	t.string 	 :citi
-    	t.string 	 :reference_url
+      t.string   :name
+      t.datetime :start_date
+      t.datetime :end_date
+      t.text     :description
+      t.integer  :type
+      t.string   :country
+      t.string   :citi
+      t.string   :reference_url
 
       t.timestamps
     end
