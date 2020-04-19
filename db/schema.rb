b984e68b3eaec52d1a64ea9581cec4390498fa6d
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-04-18 19:44:31 -0500

    Adds categories table to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_002758) do
+ActiveRecord::Schema.define(version: 2020_04_19_003919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_04_19_002758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sponsor_id"], name: "index_arrangements_on_sponsor_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "reference_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sponsor_contacts", force: :cascade do |t|
