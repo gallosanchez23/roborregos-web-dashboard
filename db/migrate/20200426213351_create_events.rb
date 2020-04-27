66d951d751eebabd7b1f3abbe14aa522b55bf471
RicardoChapaRomero <RicardoChapaRomero@users.noreply.github.com>
2020-04-27 08:40:20 -0500

    Fix code style issues and modify event model

--- db/migrate/20200426213351_create_events.rb
+++ db/migrate/20200426213351_create_events.rb
@@ -1,11 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
-    create_table :events do |t|
-      t.string :name
-      t.datetime :start_date
-      t.datetime :end_date
-      t.text :description
-      t.references :sponsor, foreign_key: true
+    create_table   :events do |t|
+          t.string :name
+        t.datetime :start_date
+        t.datetime :end_date
+            t.text :description
+      t.references :sponsor
 
       t.timestamps
     end
