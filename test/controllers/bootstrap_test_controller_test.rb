8e371c331ea39018a1737e5dd438d27786e037e4
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-04-30 22:49:33 -0500

    Deletes BootstrapTestController related files

--- test/controllers/bootstrap_test_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BootstrapTestControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get bootstrap_test_index_url
-    assert_response :success
-  end
-
-end
