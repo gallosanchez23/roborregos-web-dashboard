dffb19b414f6a2c5195a448cf130228df7c00d9b
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-04-30 19:49:30 -0500

    Fixes intentation

--- spec/models/team_member_spec.rb
+++ spec/models/team_member_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe TeamMember, type: :model do
   subject { team_member }
 
   context 'validations' do
-  	it { should validate_presence_of(:team) }
-  	it { should validate_presence_of(:member) }
-  	it { should be_valid }
+    it { should validate_presence_of(:team) }
+    it { should validate_presence_of(:member) }
+    it { should be_valid }
   end
 
   context 'associations' do
-  	it { should belong_to(:team) }
-  	it { should belong_to(:member) }
+    it { should belong_to(:team) }
+    it { should belong_to(:member) }
   end
 end
