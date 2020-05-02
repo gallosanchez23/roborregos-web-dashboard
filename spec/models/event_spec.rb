3f8afa1eda3cfa5cfcf9f594916fe790f07a66d4
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-05-02 14:04:08 -0500

    Fixes indentation

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Event, type: :model do
   subject { event }
 
   context 'validations' do
-  	it { should validate_presence_of(:name) }
-  	it { should validate_presence_of(:end_date) }
-  	it { should validate_presence_of(:start_date) }
-  	it { should be_valid }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:end_date) }
+    it { should validate_presence_of(:start_date) }
+    it { should be_valid }
   end
 
   context 'associations' do
-  	it { should belong_to(:sponsor) }
-  	it { should have_many(:event_members) }
-  	it { should have_many(:members).through(:event_members) }
+    it { should belong_to(:sponsor) }
+    it { should have_many(:event_members) }
+    it { should have_many(:members).through(:event_members) }
   end
 end
