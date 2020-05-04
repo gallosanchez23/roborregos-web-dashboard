deb5db66244aca7d201a90b451338b8fe753c7d5
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-05-03 23:10:06 -0500

    Updates rails helper and members factories

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -5,9 +5,10 @@ FactoryBot.define do
     major             { 'ITC' }
     status            { 'active' }
     username          { FFaker::Internet.user_name }
+    password          { FFaker::Internet.password }
     generation        { 2 }
     first_name        { FFaker::Name.first_name }
-    start_date      { Date.today - 3.years }
+    start_date        { Date.today - 3.years }
     fathers_last_name { FFaker::Name.last_name }
     mothers_last_name { FFaker::Name.last_name }
   end
