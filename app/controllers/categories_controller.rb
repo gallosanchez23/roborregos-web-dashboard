8da00db3df4621819144571cebd6d27211eeb935
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-08-09 19:48:15 -0500

    Adds error flash notice for create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,16 +13,19 @@ class CategoriesController < BaseController
 
   def create
     @category = Category.new(category_params)
+
     if @category.save
       redirect_to @category
     else
-      render :new
+      flash[:error] = t('messages.errors.could_not_save_member')
+      redirect_to new_category_path
     end
   end
 
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
+
     redirect_to categories_path
   end
 
