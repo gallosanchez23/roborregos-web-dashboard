9fb1f2b2f7d0705f16258b5d56ae281a35699908
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-08-09 19:13:36 -0500

    Fixes indentation issue in event helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,22 +1,22 @@
 module EventsHelper
-    def sponsor_options
-        Sponsor.pluck(:name, :id)
-    end
+  def sponsor_options
+    Sponsor.pluck(:name, :id)
+  end
 
-    def datepicker_field(form, field)
-        form.text_field(
-            field, 
-            class: 'form-control',
-            required: true, 
-            data: { 
-                'provide': 'datepicker',
-                'date-format': 'dd/mm/yyyy',
-                'date-viewMode': 'years',
-                'date-autoclose': 'true',
-                'date-todayBtn': 'linked',
-                'date-todayHighlight': 'true',
-                'date-orientation': 'bottom left'
-            }
-        ).html_safe
-    end
+  def datepicker_field(form, field)
+    form.text_field(
+      field,
+      class: 'form-control',
+      required: true,
+      data: {
+        'provide': 'datepicker',
+        'date-format': 'dd/mm/yyyy',
+        'date-viewMode': 'years',
+        'date-autoclose': 'true',
+        'date-todayBtn': 'linked',
+        'date-todayHighlight': 'true',
+        'date-orientation': 'bottom left'
+      }
+    ).html_safe
+  end
 end
